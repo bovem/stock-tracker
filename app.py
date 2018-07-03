b40acd23c7d24abd9d7022a42946c90507ee1fae
avnish98 <avnish1998pal@gmail.com>
2018-07-04 04:54:21 +0530

    Working IEX API with close price

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-#import quandl
 from iexfinance import get_historical_data, Stock
-import pandas_datareader.data as web
+import pandas as pd
 import datetime as dt
 import dash
 import dash_core_components as dcc
@@ -9,6 +8,7 @@ from dash.dependencies import Input, Output
 
 
 app = dash.Dash()
+ticker = pd.read_csv('tickers.csv', index_col="Symbol")
 
 #HTML Layout
 app.layout = html.Div(children=[
@@ -47,18 +47,20 @@ Output(component_id='output_graph', component_property='children'),
 def update_graph(input_data):
     start_date = dt.datetime(2017,1,1)
     end_date = dt.datetime.now()
-    #input_data2 = "WIKI/"+input_data
+    input_data = str(input_data).upper()
 
     #Get request for API
-    #df = get_historical_data(str(input_data),start_date=start_date, end_date=end_date, output_format='pandas')
-    df = Stock(str(input_data))
+    name = ticker.loc[input_data, "Company"] 
+    df = get_historical_data(str(input_data).upper(),start_date=start_date, end_date=end_date, output_format='pandas')
+    tick = Stock(input_data)
     
     # Graph layout
-    return dcc.Graph(
+    return [dcc.Graph(
             id='stocks_graph',
             figure={
-            'data':[{'x':end_date, 'y':df.get_close(), 'type':'dot ', 'name':input_data}],
-            'layout':{'title':input_data.upper()}})
+            'data':[{'x':df.index, 'y':df.close, 'type':'line', 'name':input_data}],
+            'layout':{'title':str(name)}}),
+            html.H1(tick.get_price())]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
