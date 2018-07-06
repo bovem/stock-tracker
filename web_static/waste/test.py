da40f66da635e63620ac294471a9d1297e250638
avnish98 <avnish1998pal@gmail.com>
2018-07-06 17:03:35 +0530

    Testing table for top gainers

--- web_static/waste/test.py
+++ web_static/waste/test.py
@@ -1,5 +1,10 @@
-import pandas_datareader.data as web
+"""import pandas_datareader.data as web
 f = web.DataReader('gs', 'iex-tops')
 
 
-print(f[:10])
+print(f[:10])"""
+
+from iexfinance import get_market_tops, get_stats_intraday
+
+#print(get_market_tops(output_format="pandas"))
+print(get_stats_intraday(output_format="pandas",symbol="AAPL"))
