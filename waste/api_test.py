daf715ecb3b9636a77aab031d1cefa1180416ace
avnish98 <avnish1998pal@gmail.com>
2018-07-08 06:45:26 +0530

    Added info page

--- waste/api_test.py
+++ waste/api_test.py
@@ -5,4 +5,6 @@ start = datetime(2017, 2, 9)
 end = datetime(2017, 5, 24)
 
 df = get_historical_data("TSLA", start=start, end=end, output_format='pandas')
+
+df.to_csv("sample_data_tsla.csv")
 print(df.tail())
